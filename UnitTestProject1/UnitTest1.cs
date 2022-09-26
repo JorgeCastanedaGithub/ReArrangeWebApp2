ad4a2d248e9b9e9ffe9b078bf54a571f4b943a07
JorgeCastanedaGithub <114272422+JorgeCastanedaGithub@users.noreply.github.com>
2022-09-26 18:27:36 -0500

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodReArrange()
+        {
+            ///Variables used for test
+            string[] arrayN = new string[] { "Amazon", "Apple", "Google" };
+            int[] arrayO = new int[] { 2, 3, 1 };
+
+            ///Set the size of the array
+            int k = arrayN.Length;
+
+            ///Create the new array
+            string[] newArray = newArray = new string[k];
+
+            //Call the WebApi
+            WebApplication3.Controllers.ReAController rea = new WebApplication3.Controllers.ReAController();
+            newArray = rea.ReArrangeArrays(arrayN, arrayO);
+        }
+    }
+}
---
+++ WebApplication3/Controllers/ReAController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class ReAController : Controller
+    {
+        public IActionResult ReArrangeArrays(string[] arrayN, int[] arrayO)
+        {
+            string[] newArray;
+
+            // Variables used for test
+            arrayN = new string[] { "Amazon", "Apple", "Google" };
+            arrayO = new int[] { 2, 3, 1 };
+
+            ///Set the size of the array
+            int k = arrayN.Length;
+
+            ///Create the new array
+            newArray = new string[k];
+            try
+            {
+                ///Reordered the values 
+                for (int j = 0; j < k; j++)
+                {
+                    string name = arrayN[j];
+                    int order = arrayO[j];
+                    newArray.SetValue(name, order - 1);
+                }
+            }
+            catch (Exception e)
+            {
+                ///For this example, if something wrong happen, show the error meesagge
+                newArray.SetValue(e.Message, 0);
+            }
+
+            return Ok(newArray);
+
+        }
+    }
+}
